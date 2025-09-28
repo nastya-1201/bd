7d9622d674a6c09594cb87dcf4151795b9ee5684
nastya-1201 <iloveenote145@gmail.com>
2025-09-28 18:21:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diet.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Простая модель пользователя
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        
+        # Проверяем, нет ли уже такого пользователя
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Пользователь с таким именем уже существует!')
+            return redirect(url_for('register'))
+        
+        # Создаем нового пользователя
+        new_user = User(username=username, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        flash('Регистрация успешна!')
+        return redirect(url_for('index'))
+    
+    return render_template('register.html')
+
+@app.route('/users')
+def users():
+    all_users = User.query.all()
+    return render_template('users.html', users=all_users)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Создаем таблицы в базе данных
     app.run(debug=True)
